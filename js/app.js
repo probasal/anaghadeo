48e71bfec386f71cab92de9f8a950566842172d1
probasal8655 <shailendrasagarsingh@yahoo.in>
2021-04-24 16:17:52 +0530

    new files

---
+++ js/app.js
@@ -0,0 +1,14 @@
+(function() {
+  var app = angular.module("showcaseApp", []);
+  var item_block, storeColor, rand;
+  app.controller("showcaseController", function($scope, $http) {
+    $http.get("data.json?nocache=" + new Date().getTime())
+      .success(function(response) {
+
+        $scope.showcase = response;
+      });
+  });
+})();
+
+function applycss() {
+}
